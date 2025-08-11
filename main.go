d024195c9e17c9730bb882c8a1e0f14a4938f4a5
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-08-10 23:42:36 -0700

    Done reading for today

--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "You requested %s", r.URL.Path)
+}
+
+func booksHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "You requested some books from path: %s", r.URL.Path)
+}
 
 func main() {
-	var message string = "This is the beginning of my golang backned journey"
-	fmt.Println(message)
+	// http.HandleFunc("/", handler)
+	// http.HandleFunc("/books", booksHandler)
+
+	// fmt.Println("Starting server on port 8080")
+	// http.ListenAndServe(":8080", nil)
+
+	mp := map[string]int{
+		"Emeka": 23,
+	}
+	mp["chidera"] = 34
+	fmt.Println(mp)
 }
